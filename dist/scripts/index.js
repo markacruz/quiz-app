ca951159e85ef998e0ca656d4cff05eb90831ea2
Mark Cruz <markaycruz@gmail.com>
2021-12-14 18:06:26 -0500

    new changes

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,79 +1,101 @@
-define(['axios', 'initial'], function() {
+define(['axios'], function() {
 
     const axios = require("axios");
+    newSelect = JSON.parse(sessionStorage.getItem("select"));
 
-    function init() {
-        let token = "lPeWERzUHt1nR89lW4AbL6OMYNwk8fj7IBtywzVi";
-        let category = selectCategory;
-        let difficulty = selectDifficulty;
-        let limit = selectLimit;
-        
-        const URL = `https://quizapi.io/api/v1/questions?apiKey=${token}&category=${category}&difficulty=${difficulty}&limit=${limit}`;
+    let token = "lPeWERzUHt1nR89lW4AbL6OMYNwk8fj7IBtywzVi";
+    let category = newSelect[0];
+    let difficulty = newSelect[1];
+    let limit = newSelect[2];
     
-        let questions = [];
-        let answers = [];
-        let correctAnswerData = [];
-        let scoreKeeper = 0;
-    
-        for (let i = 0; i < limit; i++) answers[i] = [];
-    
-        async function getQuestions() {
-            try {
-                let res = await axios.get(URL);
-                return res.data;
-    
-            } catch (err) { 
-                console.log(err); 
-            } 
-        }
-    
-        getQuestions()
-            .then((res) => {
-                for (let i = 0; i < res.length; i++) {
-                    questions[i] = res[i].question;
-                    answers[i][0] = res[i].answers.answer_a;
-                    answers[i][1] = res[i].answers.answer_b;
-                    answers[i][2] = res[i].answers.answer_c;
-                    answers[i][3] = res[i].answers.answer_d;
-                    correctAnswerData[i] = res[i].correct_answer;
-                }
-                loadQuiz();
-            });
-    
-        let numAnswered = 0;
-    
-        let question = document.getElementById("question");
-        let firstAnswer = document.getElementById("firstAnswer");
-        let secondAnswer = document.getElementById("secondAnswer");
-        let thirdAnswer = document.getElementById("thirdAnswer");
-        let fourthAnswer = document.getElementById("fourthAnswer");
-        let submitButton = document.getElementById("submit");
-        let numberOfQuestion = document.getElementById("numberAnswered");
-        let numberLimit = document.getElementById("numberLimit");
-    
-        function loadQuiz() {
-                if (numAnswered < limit) {
+    const URL = `https://quizapi.io/api/v1/questions?apiKey=${token}&category=${category}&difficulty=${difficulty}&limit=${limit}`;
+
+    let questions = [];
+    let answers = [];
+    let correctAnswerData = [];
+    let scoreKeeper = 0;
+
+    for (let i = 0; i < limit; i++) answers[i] = [];
+
+    async function getQuestions() {
+        try {
+            let res = await axios.get(URL);
+            return res.data;
+
+        } catch (err) { 
+            console.log(err); 
+        } 
+    }
+
+    getQuestions()
+        .then((res) => {
+            for (let i = 0; i < res.length; i++) {
+                questions[i] = res[i].question;
+                answers[i][0] = res[i].answers.answer_a;
+                answers[i][1] = res[i].answers.answer_b;
+                answers[i][2] = res[i].answers.answer_c;
+                answers[i][3] = res[i].answers.answer_d;
+                correctAnswerData[i] = res[i].correct_answer;
+            }
+            loadQuiz();
+        });
+
+    let numAnswered = 0;
+
+    let question = document.getElementById("question");
+    let firstAnswer = document.getElementById("firstAnswer");
+    let secondAnswer = document.getElementById("secondAnswer");
+    let thirdAnswer = document.getElementById("thirdAnswer");
+    let fourthAnswer = document.getElementById("fourthAnswer");
+    let submitButton = document.getElementById("submit");
+    let numberOfQuestion = document.getElementById("numberAnswered");
+    let numberLimit = document.getElementById("numberLimit");
+
+    function loadQuiz() {
+            if (numAnswered < limit) {
+                if (answers.length == 3) {
+                    let lastRadio = document.getElementById("fourthRadio");
+                    lastRadio.style.display = 'none';
                     question.innerHTML = questions[numAnswered];
                     firstAnswer.innerHTML = answers[numAnswered][0];
                     secondAnswer.innerHTML = answers[numAnswered][1];
                     thirdAnswer.innerHTML = answers[numAnswered][2];
-                    fourthAnswer.innerHTML = answers[numAnswered][3];
                     numberOfQuestion.innerHTML = numAnswered + 1;
                     numberLimit.innerHTML = limit;
-                } else {alert(`You got ${scoreKeeper} out of ${limit}!`)}
+                }
+                question.innerHTML = questions[numAnswered];
+                firstAnswer.innerHTML = answers[numAnswered][0];
+                secondAnswer.innerHTML = answers[numAnswered][1];
+                thirdAnswer.innerHTML = answers[numAnswered][2];
+                fourthAnswer.innerHTML = answers[numAnswered][3];
+                numberOfQuestion.innerHTML = numAnswered + 1;
+                numberLimit.innerHTML = limit;
+            } else {
+                if (scoreKeeper < limit/2) {
+                    alert(`You got ${scoreKeeper} out of ${limit}! Better luck next time.`);
+                } else {
+                    alert(`Congratulations! You got ${scoreKeeper} out of ${limit}!`);
+                }
+                window.location.href = "index.html"
             }
-    
-        submitButton.addEventListener("click", () => {
+        }
+
+    submitButton.addEventListener("click", () => {
+        try {
             let radioChecked = document.querySelector('input[name="answer"]:checked').value;
+            
+            console.log(radioChecked == correctAnswerData[numAnswered])
+            console.log(radioChecked)
             if (radioChecked == correctAnswerData[numAnswered]) scoreKeeper++;
             numAnswered++;
             document.querySelector('input[name="answer"]:checked').checked = false;
             loadQuiz();
-        });
-    }
+        }
+        catch {
+            alert("Choose an answer!")
+        }
+    });
 
-    init();
-    
 });
 
     
--- dist/scripts/initial.js
+++ dist/scripts/initial.js
@@ -3,14 +3,22 @@ let selectCategory = document.getElementById("category").value;
 let selectDifficulty = document.getElementById("difficulty").value;
 let selectLimit = document.getElementById("limit").value;
 
-function getSelectCategory() {
+let select = [selectCategory, selectDifficulty, selectLimit];
+
+function changeCategory() {
     selectCategory = document.getElementById("category").value;
+    select[0] = selectCategory;
 }
 
-function getSelectDifficulty() {
+function changeDifficulty() {
     selectDifficulty = document.getElementById("difficulty").value;
+    select[1] = selectDifficulty;
+}
+function changeLimit() {
+    selectLimit = document.getElementById("limit").value;
+    select[2] = selectLimit;
 }
 
-function getSelectLimit() {
-    selectLimit = document.getElementById("limit").value;
-}
+startButton.addEventListener("click" , () => {
+    sessionStorage.setItem("select", JSON.stringify(select));
+})
